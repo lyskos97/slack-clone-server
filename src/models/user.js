3c63932ef27ad5bf916124635be3dec756120e03
Konstantin Lyssenko <lyskos97@gmail.com>
2018-04-04 02:46:45 +0600

    refactor: password hashing via schema `afterValidate` hook

--- src/models/user.js
+++ src/models/user.js
@@ -2,35 +2,53 @@
 
 // $FlowFixMe
 import { type Sequelize, type DataTypes, type Models } from 'sequelize';
+import bcrypt from 'bcrypt';
 
 export default (sequelize: Sequelize, dataTypes: DataTypes) => {
-  const User = sequelize.define('users', {
-    username: {
-      type: dataTypes.STRING,
-      unique: true,
-      validate: {
-        isAlphanumeric: {
-          msg: 'The username can contain only letters and numbers',
+  const User = sequelize.define(
+    'users',
+    {
+      username: {
+        type: dataTypes.STRING,
+        unique: true,
+        validate: {
+          isAlphanumeric: {
+            msg: 'The username can contain only letters and numbers',
+          },
+          len: {
+            msg: 'The username must be between 3 and 35 characters long',
+            args: [3, 25],
+          },
         },
-        len: {
-          msg: 'The username must be at least 3 characters long and 25 at most',
-          args: [3, 25],
+      },
+      email: {
+        type: dataTypes.STRING,
+        unique: true,
+        validate: {
+          isEmail: {
+            msg: 'Invalid email',
+          },
+        },
+      },
+      password: {
+        type: dataTypes.STRING,
+        validate: {
+          len: {
+            msg: 'The password must be between 3 and 35 characters long',
+            args: [3, 25],
+          },
         },
       },
     },
-    email: {
-      type: dataTypes.STRING,
-      unique: true,
-      validate: {
-        isEmail: {
-          msg: 'Invalid email',
+    {
+      hooks: {
+        afterValidate: (user: Object) => {
+          // eslint-disable-next-line no-param-reassign
+          user.password = bcrypt.hashSync(user.password, 12);
         },
       },
-    },
-    password: {
-      type: dataTypes.STRING,
-    },
-  });
+    }
+  );
 
   User.associate = (models: Models) => {
     User.belongsToMany(models.Team, {
